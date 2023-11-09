a59d1ff7bfb42633b22f262ea2694ce8b94b1a9c
ifed00 <igorfedosov00@gmail.com>
2023-11-09 17:41:40 +0300

    Model Product added to catalog app.

--- ecommerce/catalog/models.py
+++ ecommerce/catalog/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from decimal import Decimal
 
-# Create your models here.
+from django.db import models
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
+
+
+def validate_percent(input_data: Decimal) -> None:
+    if input_data < 0 or input_data > 100:
+        raise ValidationError(f'{input_data = }, but should be positive and less or equal to 100',
+                              code = 'out_of_range')
+
+
+class Product(models.Model):
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    discount_percent = models.DecimalField(max_digits=5, decimal_places=2, validators=[validate_percent])
+    picture = models.ImageField(upload_to='products')
+    units_available = models.IntegerField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    published_at = models.DateTimeField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    details_content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    details_id = models.PositiveIntegerField()
+    details_object = GenericForeignKey("details_content_type", "details_id")
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["details_content_type", "details_id"]),
+        ]
--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'catalog.apps'
 ]
 
 ROOT_URLCONF = 'ecommerce.urls'
@@ -121,3 +123,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / 'media'
