934f8272695cb2bcaebcd4a67d4234463b5a73d0
ifed00 <igorfedosov00@gmail.com>
2023-11-10 19:29:28 +0300

    In admin Product page added hyperlink to corresponding admin Details entry.

--- ecommerce/catalog/admin.py
+++ ecommerce/catalog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.safestring import mark_safe
 
 from .models import Product, Category, PhoneDetails, FridgeDetails
 
@@ -8,7 +10,19 @@ from .models import Product, Category, PhoneDetails, FridgeDetails
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['name', 'price', 'discount_percent', 'units_available']
-    # TODO: add button in Product admin leading to corresponding Details object.
+    readonly_fields = ['url_to_details']
+
+    @admin.decorators.display(description='URL to details entry')
+    def url_to_details(self, product: Product):
+        meta = product.details_object._meta  # Django documentation implies _meta is public member
+        if product.details_id is not None:
+            url = reverse(f'admin:{meta.app_label}_{meta.model_name}_change', args=[product.details_id])
+            text = f'{meta.model_name}(id={product.details_id})'
+        else:
+            url = reverse(f'admin:{meta.app_label}_{meta.model_name}_add')
+            text = f'Add new {meta.model_name}'
+
+        return mark_safe(f'<a href={url}>{text}</a>')
 
 
 admin.site.register(Category)
