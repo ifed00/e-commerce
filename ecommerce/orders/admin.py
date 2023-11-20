9d7b70fada753ba028eb4898f816fa56a2053b4f
ifed00 <igorfedosov00@gmail.com>
2023-11-20 18:33:52 +0300

    Added AdminModels for orders app

--- ecommerce/orders/admin.py
+++ ecommerce/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Order, OrderProducts
+
 # Register your models here.
+
+
+class OrderProductsInline(admin.TabularInline):
+    model = OrderProducts
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'created_at']
+    inlines = [OrderProductsInline]
+
+
+@admin.register(OrderProducts)
+class OrderProductsAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order_id']
+    list_display_links = ['id', 'order_id']
